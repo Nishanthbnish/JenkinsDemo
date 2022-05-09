479604a93799b077d9cd3df3e9ee8bdc6f1ad43a
Nishanthbnish <bnishanthc@gmai.com>
2022-05-09 13:09:03 +0530

    img

--- JenkinsDemo/Controllers/HomeController.cs
+++ JenkinsDemo/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace JenkinsDemo.Controllers
         {
             return View();
         }
+        public IActionResult NNBS()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
